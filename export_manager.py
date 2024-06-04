2c6c264772deb75ed1f74d67e27ab53a45c742b1
Kevin Leonardo Fajardo Soto <kevinfajardosoto12@gmail.com>
2024-06-04 17:22:25 -0600

    Update export_manager.py

--- export_manager.py
+++ export_manager.py
@@ -1,13 +1,14 @@
 import json
 import logging
-# import csv <- to be implemented
-# import xml <- to be implemented
+import csv
+import xml.etree.ElementTree as ET
+
 from datetime import datetime
+from colors import Colors
 
-# TODO: Implement XML, CSV, and see which other formats could be useful
-# TODO: Study more these libraries
 
-class ExportManager():
+class ExportManager:
+
     def __init__(self):
         self.export_strategies = {
             "csv": self.export_to_csv,
@@ -19,31 +20,33 @@ class ExportManager():
         self.logger = logging.getLogger("ExportManager")
         self.logger.info("ExportManager component initialized.")
 
-    def verify_export_format(self, format):
+    def verify_export_format(self, format_str):
         """
         Verifies that the format is valid.
-        :param format: [String] Format to be used.
+        :param format_str: [String] Format to be used.
         :return: Boolean
         """
 
-        if format.strip() not in self.export_strategies:
-            self.logger.error(f"Unsupported format: {format}")
+        if format_str.strip() not in self.export_strategies:
+            message = f"Unsupported format: {format_str}"
+            self.logger.error(message)
+            print(f"{Colors.FAIL}{message}{Colors.ENDC}")
             return False
-        self.logger.info(f"Verified format successfully: {format}")
+        self.logger.info(f"Verified format successfully: {format_str}")
         return True
 
-    def export(self, events, format):
+    def export(self, events, format_str):
         """
         Export a list of events in the given format.
         Selects the correct function from 'export_strategies' for
         the given format
         :param events: [List] The events to be exported.
-        :param format: [String] The format to export the events
+        :param format_str: [String] The format to export the events
         """
 
-        if not self.verify_export_format(format):
+        if not self.verify_export_format(format_str):
             return
-        self.export_strategies[format](events)
+        self.export_strategies[format_str](events)
         # Crazy line of code here
         # It replaces the line with the needed function from 'export_strategies'
 
@@ -52,8 +55,10 @@ class ExportManager():
         Receives events but doesn't export them. Useful for testing.
         :param events: [List] Events to be exported
         """
-        print("The export format is 'none' so nothing is exported, the events are only printed.")
-        self.logger.info(f"{(len(list(events)))} events were provided but not exported. Format = 'none'.")
+
+        message = f"The export format is 'none' so nothing was exported."
+        print(f"{Colors.OKGREEN}{message}{Colors.ENDC}")
+        self.logger.info(f"{(len(list(events)))} events were provided. {message}")
 
     def export_to_json(self, events):
         """
@@ -67,8 +72,10 @@ class ExportManager():
         filename = f"events_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
         with open(filename, "w") as file:
             json.dump(events, file, indent=4)
-        print(f"Events exported to {filename}")
-        self.logger.info(f"{(len(list(events)))} events exported to {filename} in JSON format.")
+
+        message = f"Events exported to {filename}"
+        print(f"{Colors.OKGREEN}{message}{Colors.ENDC}")
+        self.logger.info(f"{(len(list(events)))} {message.lower()}")
 
     def export_to_log(self, events):
         """
@@ -83,12 +90,52 @@ class ExportManager():
         with open(filename, "w") as file:
             for event in events:
                 file.write(f"{datetime.now()} - {json.dumps(event)}\n")
-            print(f"Events exported to {filename}")
-            self.logger.info(f"{(len(list(events)))} events exported to {filename} in LOG format.")
 
-    # TODO:
+        message = f"Events exported to {filename}"
+        print(f"{Colors.OKGREEN}{message}{Colors.ENDC}")
+        self.logger.info(f"{(len(list(events)))} {message.lower()}")
+
     def export_to_csv(self, events):
-        print("Export to CSV: To be implemented... soon")
+        """
+        Exports list of events to a file in csv format
+        :param events: [List] Events to be exported
+        """
+
+        # If file doesn't exist, it creates a new one
+        filename = f"events_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+        with open(filename, "w", newline='') as file:
+            writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            # Write header based on event keys
+            if events:
+                header = events[0].keys()
+                writer.writerow(header)
+                for event in events:
+                    writer.writerow(event.values())
+
+        message = f"Events exported to {filename}"
+        print(f"{Colors.OKGREEN}{message}{Colors.ENDC}")
+        self.logger.info(f"{(len(list(events)))} {message.lower()}")
+
     def export_to_xml(self, events):
-        print("Export to XML: To be implemented... soon")
-      
+        """
+        Exports list of events to a file in xml format
+        :param events: [List] Events to be exported
+        """
+
+        # If file doesn't exist, it creates a new one
+        filename = f"events_{datetime.now().strftime('%Y%m%d_%H%M%S')}.xml"
+
+        root = ET.Element("Events")
+        for event in events:
+            event_element = ET.Element("Event")
+            for key, value in event.items():
+                child = ET.SubElement(event_element, key)
+                child.text = str(value)
+            root.append(event_element)
+
+        tree = ET.ElementTree(root)
+        tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+        message = f"Events exported to {filename}"
+        print(f"{Colors.OKGREEN}{message}{Colors.ENDC}")
+        self.logger.info(f"{(len(list(events)))} {message.lower()}")
